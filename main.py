1f65ce352f52c169332574872cd5e539bd1a2c7a
Lacasse <Junior_julescesar10@hotmail.com>
2024-02-21 17:19:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/popup')
+def show_popup():
+    return render_template('popup.html')
+
+
+@app.route('/move_button', methods=['POST'])
+def move_button():
+    x = random.randint(0, 350)
+    y = random.randint(0, 350)
+    return {'x': x, 'y': y}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
